25e55a8847b9ea15c4c99274de02ec61dd5a8554
jimjam <jmusgrave@orcon.net.nz>
2010-09-18 15:21:44 +1200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  root :to => 'pahes#home'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
